2ee89b16adb16fba0713358509a17150b5967431
HardeepSinghAu <11919@coderacademy.edu.au>
2022-10-10 08:42:17 +1100

    installation of tailwind package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Nav from './Components/Nav'
 import About from './Components/About'
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Nav />
       </header>
       <About />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (  
+        <footer>
+            <p>Copyright © HSingh websites 2022 </p>
+        </footer>
+
+    );
+}
+ 
+export default Footer;
