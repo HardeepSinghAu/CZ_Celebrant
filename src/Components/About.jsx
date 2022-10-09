b7c4ef9fc9dc3816e7691d8d427f44c078180631
HardeepSinghAu <11919@coderacademy.edu.au>
2022-10-09 19:24:28 +1100

    initial commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+    return (
+        <>
+            <h1>Courtney Zagami Celebrations 🪴</h1>
+                <h3>Authorised Marriage Celebrant ❤️🌈</h3>
+                <span>🔹Weddings 🔹 Baby naming 🔹 Funerals 🔹Celebrations of life 🔹 Unique ceremonies 🔹Professional storytelling.</span>
+            <span> Let me celebrate you. ✨</span>
+            <p>Phone: 0405811598
+Email: courtneyzagami@outlook.com </p>
+            <p>
+            My name is Courtney and I am an authorised marriage celebrant… also known as professional storyteller, curious and compassionate listener and enthusiastic lover of humans and life. 
+
+I’m married and a mama to three little people, as well as having a career as a Psychologist. While I don’t provide psychological services as part of my celebrant work, I believe it makes me uniquely qualified to listen, understand and relay your love, and loss, stories. 
+
+Why become a Celebrant with all that going on? Well, I delight in hearing people’s stories and celebrating joy, creative writing, public speaking and I even love paperwork! 
+
+My books are open now for all manner of ceremonies, from weddings to baby naming ceremonies, funerals to vow renewals and everything in between. 
+
+All stages of life are worth celebrating, so what are you waiting for? Get in touch today! ✨
+            </p>
+        
+        </>
+      );
+}
+ 
+export default About;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return ( 
+        <nav>
+            <a href='/'>About</a>
+            <a href='/'>Contact</a>
+        </nav>
+     );
+}
+ 
+export default Nav;
